40b50575e49d88fd819aeb1b9a3dfe85a46cc6cd
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-11-26 11:18:23 +0530

    implemented sort order and status api

--- database/models/headerFooter.js
+++ database/models/headerFooter.js
@@ -9,19 +9,68 @@ module.exports = (sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      header_logo: {
+      header_logo_media_path: {
         type: DataTypes.TEXT,
         allowNull: true,
       },
-      footer_logo: {
+      footer_logo_media_path: {
         type: DataTypes.TEXT,
         allowNull: true,
       },
-      logo_alt: {
+      header_media_alt: {
         type: DataTypes.STRING,
         allowNull: true,
         defaultValue: "",
       },
+
+      footer_media_alt: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      language:{
+        type: DataTypes.ENUM,
+        values: ['en', 'ar'],
+        allowNull: false,
+        defaultValue: 'en',
+      },
+
+      address:{
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      sale_enquiry_title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      sale_enquiry_email:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      support_email:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      phone_number:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      news_letter_title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      po_box_number:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      
+
     },
     {
       tableName: "header_footer",

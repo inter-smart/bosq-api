389f6719e4d575031e7b9ca49abc711858a66b4f
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-12-09 17:40:29 +0530

    cerated customisation cms model

---
+++ database/models/cms/customization/customizationCms.js
@@ -0,0 +1,146 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const CustomizationCms = sequelize.define(
+    "CustomizationCms",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      title_ar:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      banner_media_desktop_path:{
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      banner_media_mobile_path:{
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      banner_media_alt: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      banner_media_alt_ar: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+
+      banner_title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      banner_title_ar:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },    
+
+      banner_description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      banner_description_ar: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      process_title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      process_title_ar:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      process_media_path:{
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      process_media_alt:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      process_media_alt_ar: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+
+     
+      options_title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      options_title_ar: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+
+      options_description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      options_description_ar: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      form_title:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      form_title_ar:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      form_description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      form_description_ar: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      form_media_path: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      form_media_alt: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      form_media_alt_ar: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "customization_cms",
+    }
+  );
+
+  return CustomizationCms;
+};

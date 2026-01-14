7a9f61ab00f14124be65f6d1df3cc23dae127b4b
afsalmern <afsal@intersmart.in>
2026-01-14 16:53:31 +0530

    redis upated

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,7 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  },
-  password: process.env.REDIS_PASSWORD,
+  url: process.env.REDIS_URL,
 });
 
 redisClient.on("connect", () => {

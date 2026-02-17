dc6d39a4ea848e662222983665e2a2dd57c30499
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-02-17 18:39:41 +0530

    order details api

--- modules/admin/http/controllers/orders/OrderController.js
+++ modules/admin/http/controllers/orders/OrderController.js
@@ -1,13 +1,13 @@
 const { validationResult } = require("express-validator");
 const {
-  sequelize,
-  models,
+    sequelize,
+    models,
 } = require("../../../../../database/models/index.js");
 const {
-  sendValidationError,
-  sendSuccessResponse,
-  sendErrorResponse,
-  sendNotFoundError,
+    sendValidationError,
+    sendSuccessResponse,
+    sendErrorResponse,
+    sendNotFoundError,
 } = require("../../traits/responseHandler.js");
 
 const { Op, literal } = require("sequelize");
@@ -18,81 +18,100 @@ const { paginate } = require("../../traits/datatablePaginationHelper.js");
 const DataModel = models.Orders;
 
 class OrderController {
-  static async index(req, res) {
-    try {
-      const result = await paginate(DataModel, req, {
-        order: [["createdAt", "DESC"]],
-        include: [
-          {
-            model: models.Users,
-            as: "user",
-            attributes: [
-              "id",
-              "first_name",
-              "last_name",
-              "email",
-              [
-                literal(
-                  `CONCAT_WS(' ', "user"."country_code", "user"."mobile")`,
-                ),
-                "mobile",
-              ],
-            ],
-          },
-        ],
-        searchFields: ["order_id", "email"],
-      });
+    static async index(req, res) {
+        try {
+            const result = await paginate(DataModel, req, {
+                order: [["createdAt", "DESC"]],
+                include: [
+                    {
+                        model: models.Users,
+                        as: "user",
+                        attributes: [
+                            "id",
+                            "first_name",
+                            "last_name",
+                            "email",
+                            [
+                                literal(
+                                    `CONCAT_WS(' ', "user"."country_code", "user"."mobile")`,
+                                ),
+                                "mobile",
+                            ],
+                        ],
+                    },
+                ],
+                searchFields: ["order_id", "email"],
+            });
 
-      const response = {
-        list: result.data,
-        pagination: result.pagination,
-      };
+            const response = {
+                list: result.data,
+                pagination: result.pagination,
+            };
 
-      sendSuccessResponse(res, response, "Orders retrieved successfully");
-    } catch (error) {
-      console.error("Order index error:", error);
-      sendErrorResponse(res, error);
-    }
-  }
-
-  static async show(req, res) {
-    await Promise.all(validateId.map((validation) => validation.run(req)));
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return sendValidationError(res, errors.array());
+            sendSuccessResponse(res, response, "Orders retrieved successfully");
+        } catch (error) {
+            console.error("Order index error:", error);
+            sendErrorResponse(res, error);
+        }
     }
 
-    try {
-      const { id } = req.params;
+    static async show(req, res) {
+        await Promise.all(validateId.map((validation) => validation.run(req)));
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return sendValidationError(res, errors.array());
+        }
 
-      const data = await DataModel.findByPk(id, {
-        include: [
-          {
-            model: models.Users,
-            as: "user",
-            attributes: ["id", "first_name", "last_name", "email", "mobile"],
-          },
-          {
-            model: models.OrderItem,
-            as: "items",
-          },
-          {
-            model: models.OrderAddress,
-            as: "addresses",
-          },
-        ],
-      });
+        try {
+            const { id } = req.params;
 
-      if (!data) {
-        return sendNotFoundError(res, "Order");
-      }
+            const data = await DataModel.findByPk(id, {
+                include: [
+                    {
+                        model: models.Users,
+                        as: "user",
+                        attributes: ["id", "first_name", "last_name", "email", "mobile"],
+                    },
+                    {
+                        model: models.OrderItem,
+                        as: "items",
+                        include: [
+                            {
+                                model: models.ProductBase,
+                                as: "product",
+                                attributes: ["id", "title", "media_path"],
+                            },
+                            {
+                                model: models.ProductVariants,
+                                as: "variant",
+                                attributes: ["id", "sku", "title", "media_path", "price"],
+                            },
+                        ],
+                    },
+                    {
+                        model: models.OrderAddress,
+                        as: "addresses",
+                        include: [
+                            {
+                                model: models.State,
+                                as: "state",
+                                attributes: ["id", "name"],
+                            },
+                        ],
+                    },
+                ],
+            });
 
-      sendSuccessResponse(res, data, "Order retrieved successfully");
-    } catch (error) {
-      console.error("Order show error:", error);
-      sendErrorResponse(res, error);
+            if (!data) {
+                return sendNotFoundError(res, "Order");
+            }
+
+            sendSuccessResponse(res, data, "Order retrieved successfully");
+        } catch (error) {
+            console.error("Order show error:", error);
+            sendErrorResponse(res, error);
+        }
     }
-  }
 }
 
 module.exports = OrderController;

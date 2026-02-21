89d1fb21174e375e8600e974165d924cec48eb5d
afsalmern <afsal@intersmart.in>
2026-02-21 23:31:14 +0530

    added login with google

--- modules/frontend/routes/auth.js
+++ modules/frontend/routes/auth.js
@@ -8,8 +8,8 @@ router.post("/verify-otp", Controller.verifyOtp);
 router.post("/create-password", verifyTempToken, Controller.createPassword);
 
 router.post("/login", Controller.login);
+router.post("/google-login", Controller.googleLogin);
 router.post("/forgot-password", Controller.forgotPassword)
 router.post("/verify-reset-password-otp",Controller.verifyForgotPasswordOtp)
-router.post("/reset-password",verifyTempToken, Controller.createNewPassword)
 router.post("/reset-password",verifyTempToken, Controller.createNewPassword)
 module.exports = router;

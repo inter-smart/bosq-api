71646190eff105a31847d24b8db36d10f39aeb3b
afsalmern <afsal@intersmart.in>
2026-03-06 10:46:20 +0530

    removed cache code as its not used

--- modules/frontend/http/services/siteSettingsService.js
+++ modules/frontend/http/services/siteSettingsService.js
@@ -1,6 +1,4 @@
 const { models } = require("../../../../database/models");
-const cacheKeys = require("../../../redis/cacheKeys");
-const { setCache, getCache } = require("../../../redis/redisService");
 const {
   buildHeaderSection,
   buildFooterSection,
@@ -9,72 +7,56 @@ const {
   buildPaymentCards,
 } = require("../traits/dataManipulations/siteSettings");
 
-const cacheKey = cacheKeys.siteSettings;
-
 class SiteSettingsService {
   static async getData() {
     try {
-      // 1. Get data from cache
-      const cachedData = await getCache(cacheKey);
+      const [siteSettings, socialLinks, paymentMethods, products, projects, landingPage] = await Promise.all([
+        await models.HeaderFooter.findOne(),
+        await models.SocialMedia.findAll({
+          where: {
+            status: true,
+          },
+          order: [["sort_order", "ASC"]],
+        }),
+        await models.PaymentMethods.findAll({
+          where: {
+            status: true,
+          },
+          order: [["sort_order", "ASC"]],
+        }),
+        await models.ProductCategory.findAll({
+          where: {
+            status: true,
+            parent_id: null,
+          },
+          attributes: ["id", "name", "name_ar", "media_path", "slug"],
+          include: [
+            {
+              model: models.ProductCategory,
+              as: "children",
+              attributes: ["id", "name", "name_ar", "media_path", "slug"],
+              where: { status: true },
+              required: false,
+            },
+          ],
+          order: [["sort_order", "ASC"]],
+        }),
+        await models.Projects.findAll({
+          where: {
+            status: true,
+          },
+          attributes: ["title", "title_ar", "thumbnail", "slug"],
+          order: [["sort_order", "ASC"]],
+        }),
 
-      // 2. If cached data exists, return it
-      // if (cachedData) {
-      //   return {
-      //     data: cachedData,
-      //     fromCache: true,
-      //     message: "Site settings data fetched from cache",
-      //   };
-      // }
-
-      //   3. If no cached data, fetch from database
-      const [siteSettings, socialLinks, paymentMethods, products, projects, landingPage] =
-        await Promise.all([
-          await models.HeaderFooter.findOne(),
-          await models.SocialMedia.findAll({
-            where: {
-              status: true,
-            },
-            order: [["sort_order", "ASC"]],
-          }),
-          await models.PaymentMethods.findAll({
-            where: {
-              status: true,
-            },
-            order: [["sort_order", "ASC"]],
-          }),
-          await models.ProductCategory.findAll({
-            where: {
-              status: true,
-              parent_id: null
-            },
-            attributes: ["id", "name", "name_ar", "media_path", "slug"],
-            include: [
-              {
-                model: models.ProductCategory,
-                as: "children",
-                attributes: ["id", "name", "name_ar", "media_path", "slug"],
-                where: { status: true },
-                required: false
-              },
-            ],
-            order: [["sort_order", "ASC"]],
-          }),
-          await models.Projects.findAll({
-            where: {
-              status: true,
-            },
-            attributes: ["title", "title_ar", "thumbnail", "slug"],
-            order: [["sort_order", "ASC"]],
-          }),
-
-          await models.LandingPage.findAll({
-            where: {
-              status: true,
-            },
-            attributes: ["title", "title_ar","slug"],
-            order: [["sort_order", "ASC"]],
-          }),
-        ]);
+        await models.LandingPage.findAll({
+          where: {
+            status: true,
+          },
+          attributes: ["title", "title_ar", "slug"],
+          order: [["sort_order", "ASC"]],
+        }),
+      ]);
 
       const headerData = buildHeaderSection(siteSettings);
       const footerData = buildFooterSection(siteSettings);
@@ -89,13 +71,9 @@ class SiteSettingsService {
         cards,
         navigationData,
         products,
-        landingPage
+        landingPage,
       };
 
-      //   5. Store the result in cache for future requests
-      await setCache(cacheKey, result);
-
-      //   6. Return the result
       return {
         data: result,
         message: "Site settings data fetched",

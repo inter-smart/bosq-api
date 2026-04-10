8d83ac44d40623bfabb2569a0863f8b7afbad0c7
afsalmern <afsal@intersmart.in>
2026-04-10 11:46:49 +0530

    added dashboard controller

--- modules/admin/routes/dashboard.js
+++ modules/admin/routes/dashboard.js
@@ -6,5 +6,9 @@ const authMiddleware = require("../http/middleware/authMiddleware");
 router.use(authMiddleware(["admin"]));
 
 router.get('/counts', DashboardController.getCounts);
+router.get('/order-stats', DashboardController.getOrderStats);
+router.get('/product-stats', DashboardController.getProductStats);
+router.get('/coupon-analytics', DashboardController.getCouponAnalytics);
+router.get('/user-stats', DashboardController.getUserStats);
 
 module.exports = router;

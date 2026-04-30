44a3b10baa901e4c797a1301cda479a85a6c75e1
afsalmern <afsal@intersmart.in>
2026-04-30 13:33:09 +0530

    changed the logout api from auth middleware

--- modules/frontend/routes/profile.js
+++ modules/frontend/routes/profile.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const Controller = require("../http/controllers/userController.js");
 const { verifyToken } = require("../http/middleware/authMiddleware.js");
 
+router.post("/logout", Controller.logout);
+
 router.use(verifyToken());
 router.get("/my-profile", Controller.getProfileData);
 router.put("/edit-profile", Controller.editProfile);
 router.get("/fetch-profile-by-id", Controller.fetchProfileById);
 router.put("/change-password", Controller.changePassword);
-router.post("/logout", Controller.logout);
 
 module.exports = router;
